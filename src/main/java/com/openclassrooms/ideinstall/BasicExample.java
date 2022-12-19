e85a08ba63810ad6e7f85181233c8db4839509d9
NtechVDH <NtechVDH@Admins-PC>
2022-12-19 14:39:00 +0100

    correction de bug

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,7 +3,8 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int i; // Correction en retirant final et index pour mettre i
+		int i; // Correction en retirant final et index pour mettre i //
+		// attention au token
 		for (i = 0; i < 3; i++) { // correction de la boucle infini en limitant à 3
 			System.out.println("Hello bugs !");
 		}
