53e604fe9453a81b5912dda8a661067220f464bf
NtechVDH <NtechVDH@Admins-PC>
2022-12-19 14:48:05 +0100

    rebelotte

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,6 +4,7 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i; // Correction en retirant final et index pour mettre i //
+		// rebelotte
 		for (i = 0; i < 3; i++) { // correction de la boucle infini en limitant à 3
 			System.out.println("Hello bugs !");
 		}
