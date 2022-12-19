9173743d70dbb6b6e0d694fb679aeaf5b12ca1fe
NtechVDH <NtechVDH@Admins-PC>
2022-12-19 14:31:32 +0100

    Correction de syntaxe et boucle infinie

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,8 +3,8 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		int i;
-		for (i = 0; i < 3; i++) {
+		int i; // Correction en retirant final et index pour mettre i
+		for (i = 0; i < 3; i++) { // correction de la boucle infini en limitant à 3
 			System.out.println("Hello bugs !");
 		}
 	}
