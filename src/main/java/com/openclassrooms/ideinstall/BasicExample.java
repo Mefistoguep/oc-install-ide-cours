3540d72ecbc540b19af7428d0b11813e013afcdd
NtechVDH <NtechVDH@Admins-PC>
2022-12-19 14:44:19 +0100

    correctino mineure

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -4,7 +4,6 @@ public class BasicExample {
 
 	public static void main(String[] args) {
 		int i; // Correction en retirant final et index pour mettre i //
-		// attention au token
 		for (i = 0; i < 3; i++) { // correction de la boucle infini en limitant à 3
 			System.out.println("Hello bugs !");
 		}
